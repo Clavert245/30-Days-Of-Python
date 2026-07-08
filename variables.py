93187f40bea536b69648942a1a6c7b2d619ff8c0
Clavert Kwarteng <133221241+Clavert245@users.noreply.github.com>
2026-07-08 19:49:31 +0000

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,12 @@
+#Day 2: 30 days of python programming
+first_name = 'Clavert'
+last-name = 'Kwarteng'
+full_name = ('first_name', 'Last_name')
+country = 'Ghana'
+city = 'kumasi'
+age = 24
+is_married = True
+year = 2001
+is_true = True
+is_light_on = True
+Occupation, car, favorite_footballer ='Vision Scientist', 'Ford F150','Messi'
